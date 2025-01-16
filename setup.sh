aba66cd2bdd8f7b124ef6c8d6e79c10e5c7794b8
xize <glucassen@live.nl>
2025-01-16 19:07:26 +0100

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+read -p "Do you want to fetch/pull submodules globally ? (submodule.recurse=true?) Y/n: " yn 
+
+if [ "$yn" = "y" -o "$yn" = "Y" ]; then
+	echo "setting to true."
+	git config --global submodule.recurse true
+else
+	echo "settings to false."
+	git config --global submodule.recurse false
+fi
+
+echo "ensure, all git module feeds are setup."
+git submodule update --init --recursive
+git submodule foreach --recursive git fetch
+git submodule foreach git merge origin master
+
+exit 0
