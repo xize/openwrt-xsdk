eb0aa2f8eea95b7c91190d4db6b22a150f925867
xize <glucassen@live.nl>
2025-05-10 16:49:16 +0200

    [setup] fix alot of issues, now correctly checking out.

--- setup.sh
+++ setup.sh
@@ -6,23 +6,27 @@ then
   git config --global submodule.recurse true
   git submodule update --init --recursive
   git submodule foreach --recursive git fetch
-  git submodule foreach git merge origin master
-  git submodule foreach git merge origin main
-  if [ "$2" = "dumbap" ]
+  git submodule foreach git merge origin master 
+  git submodule foreach git merge origin main 
+  if [ "$2" = "dumbap" ];
   then
-    cd xsdk-uci-defaults
-    git checkout dumbap
-    cp -rf files ../files
-  elif
-    cp -rf xsdk-uci-defaults/files files
+   cd xsdk-uci-defaults
+   git checkout dumbap --force
+   rm -rf ../files
+   cp -rf files ../files
+  else
+   cd xsdk-uci-defaults
+   git checkout main --force
+   rm -rf ../files
+   cp -rf files ../files
   fi
-  
   exit 0
 fi
 
 read -p "Do you want to fetch/pull submodules globally ? (submodule.recurse=true?) Y/n: " yn 
 
-if [ "$yn" = "y" -o "$yn" = "Y" ]; then
+if [ "$yn" = "y" -o "$yn" = "Y" ];
+then
 	echo "setting to true."
 	git config --global submodule.recurse true
 else
@@ -36,13 +40,17 @@ git submodule foreach --recursive git fetch
 git submodule foreach git merge origin master
 git submodule foreach git merge origin main
 
-if [ "$1" = "dumbap" ]
+if [ "$1" = "dumbap" ];
 then
   cd xsdk-uci-defaults
-  git checkout dumbap
+  git checkout dumbap --force
+  rm -rf ../files
   cp -rf files ../files
-elif
-  cp -rf xsdk-uci-defaults/files files
+else
+  cd xsdk-uci-defaults
+  git checkout main --force
+  rm -rf ../files
+  cp -rf files ../files
 fi
 
 exit 0
