823d6e8e2e1bcb77b12e5dc0484ce406a7200cf6
xize <glucassen@live.nl>
2025-12-11 14:56:24 +0100

    [script]: add new setup.sh
    have still some work to do probably

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+fw_uri="https://www.devolo.global/fileadmin/Web-Content/DE/products/hnw/magic-2-wifi-next/firmware/delos_magic-2-wifi-next_6.0.1_2023-09-06.bin"
+
+if [ ! -e "devolo.bin" -o ! -d ghn ];
+then
+	echo "devolo.bin not found, downloading now."
+	wget -q $fw_uri -O devolo.bin
+	if [[ $? -ne 0 ]];
+	then
+		echo "Error: failed to download devolo.bin, url has been changed?"
+		exit 1
+	else
+		echo "downloaded devolo.bin successfully."
+		echo "extracting devolo.bin to ghn."
+		./scripts/devolo-extract-ghn-packages devolo.bin ghn
+	fi
+else
+	echo "skip downloading devolo firmware, already found locally scoped."
+fi
+
+
+cp profiles/devolo-magic2-next.profile .config
+make defconfig
+
+exit 0
+
