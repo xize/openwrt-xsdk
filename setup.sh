4c024ca7e2f822939be6ca68151d2778a8a5cfd5
xize <glucassen@live.nl>
2026-01-27 18:34:54 +0100

    [setup]: fix some logic errors

--- setup.sh
+++ setup.sh
@@ -8,11 +8,9 @@ echo "bypassing interraction for luci git submodules ;-)"
 		rm -rf xsdk-uci-defaults
 		git clone https://github.com/xize/xsdk-uci-defaults
 		cd xsdk-uci-defaults
-		cp -rf files ../
-		cd ..
-		cd files
 		git fetch origin dumbap
 		git checkout dumbap --force
+		cp -rf files ../
 		cd ..
 		chmod -R 755 files
 	else
@@ -21,6 +19,7 @@ echo "bypassing interraction for luci git submodules ;-)"
 		git clone https://github.com/xize/xsdk-uci-defaults
 		cd xsdk-uci-defaults
 		cp -rf files ../
+		cd ..
 		chmod -R 755 files
 	fi
 exit 0
