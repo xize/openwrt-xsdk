2242a42dd4ca430e41c406cfc4e06c99f02ab63e
xize <glucassen@live.nl>
2026-01-27 13:14:41 +0100

    [setup.sh]: simplify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,58 +1,20 @@
 #!/bin/sh
 
-if [ "$1" = "skip" ];
-then
-  echo "bypassing interraction for luci git submodules ;-)"
-  git config --global submodule.recurse true
+echo "bypassing interraction for luci git submodules ;-)"
    
-  if [ "$2" = "dumbap" ];
-  then
-   git submodule set-branch -b dumbap xsdk-uci-defaults
-   git submodule sync
-   git submodule update --init --remote dumbap
-   rm -rf files
-   cp -rf xsdk-uci-defaults files
-   chmod -R 777 files
-  else
-   git submodule set-branch -b main xsdk-uci-defaults
-   git submodule sync
-   git submodule update --init --remote main
-   rm -rf files
-   cp -rf xsdk-uci-defaults files
-   chmod -R 777 files
-  fi
-  exit 0
-fi
-
-read -p "Do you want to fetch/pull submodules globally ? (submodule.recurse=true?) Y/n: " yn 
-
-if [ "$yn" = "y" -o "$yn" = "Y" ];
-then
-	echo "setting to true."
-	git config --global submodule.recurse true
-else
-	echo "settings to false."
-	git config --global submodule.recurse false
-fi
-
-if [ "$1" = "dumbap" ];
-then
-	git submodule set-branch -b dumbap
-	git submodule sync
-	git submodule update --remote --init xsdk-uci-defaults
-	rm -rf files
-	cp -rf xsdk-uci-defaults files
-	chmod -R 777 files
-	exit 0
-else
-	git submodule set-branch -b main
-	git submodule sync
-	git submodule update --remote --init xsdk-uci-defaults
-	rm -rf files
-	cp -rf xsdk-uci-defaults files
-	chmod -R 777 files
-	exit 0
-fi
-
+	if [ "$1" = "dumbap" ];
+	then
+		rm -rf files
+		git clone https://github.com/xize/xsdk-uci-defaults files
+		cd files
+		git fetch origin
+		git checkout dumbap --force
+		cd ..
+		chmod -R 755 files
+	else
+		rm -rf files
+		git clone https://github.com/xize/xsdk-uci-defaults files
+		chmod -R 755 files
+	fi
 exit 0
 
